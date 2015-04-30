ae920c36c4726ce34b2c5f37ad2642af2cf11b50
elle <mdegges@umail.iu.edu>
2015-04-29 23:07:05 -0400

    updates

--- Tester/src/servlets/CheckoutServlet.java
+++ Tester/src/servlets/CheckoutServlet.java
@@ -29,115 +29,120 @@ import database.ParsedTotal;
 public class CheckoutServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	final static String db_table = "information";
-	
+
 	static Connection conn;
 	static Statement stmt = null;
 	static BufferedReader reader = null;
-	
+
 	static String username;
 	static String password;
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");    
-        PrintWriter out = response.getWriter(); 
-        
-        String option = request.getParameter("option");
-        String cardNumber = request.getParameter("checkOut");
-        
-        try {
-        	//connect to db
-        	DBAccess connect = DBAccess.getInstance();
-        	conn=connect.getConnection();
-        	
-        	HttpSession session = request.getSession(false);  
-        	username = session.getAttribute("username").toString();
-        	password = session.getAttribute("password").toString();
-        	String cart = session.getAttribute("cart").toString();
-        	getTotal(cart);
-        	double total1 = getTotal(cart);
-        	System.out.println(total1);
-        	Context context = new Context(new ParsedTotal());
-        	double tax = context.executeStrategy(total1, .09);//tax is .15
-        	context = new Context(new IncludeTax());
-        	double totalPrice2 = context.executeStrategy(total1, tax);
-        	System.out.print(totalPrice2);
-        	request.setAttribute("totalPrice2", totalPrice2);
-        	
-        	if (option.equals("credit")){
-        		
-        		String query = "SELECT * FROM " + db_table + " WHERE username = ? AND PASSWORD = ?";
-        		PreparedStatement pstmt = (PreparedStatement) conn.prepareStatement(query); 
-        		pstmt.setString(1, username);
-        		pstmt.setString(2, password);
-        		ResultSet rs = pstmt.executeQuery();
-        		
-        		if (rs.next()){
-        			double credit = Double.parseDouble(rs.getString("store_credit"));
-        			if (credit > totalPrice2){
-        				RequestDispatcher rd = request.getRequestDispatcher("congrats.jsp");
-        				rd.forward(request, response);
-        				/*
+		response.setContentType("text/html");    
+		PrintWriter out = response.getWriter(); 
+
+		String option = request.getParameter("option");
+		String cardNumber = request.getParameter("checkOut");
+
+		try {
+			//connect to db
+			DBAccess connect = DBAccess.getInstance();
+			conn=connect.getConnection();
+
+			HttpSession session = request.getSession(false);  
+			username = session.getAttribute("username").toString();
+			password = session.getAttribute("password").toString();
+			String cart = session.getAttribute("cart").toString();
+			getTotal(cart);
+			double total1 = getTotal(cart);
+			System.out.println(total1);
+			Context context = new Context(new ParsedTotal());
+			double tax = context.executeStrategy(total1, .09);//tax is .15
+			context = new Context(new IncludeTax());
+			double totalPrice2 = context.executeStrategy(total1, tax);
+			System.out.print(totalPrice2);
+			request.setAttribute("totalPrice2", totalPrice2);
+
+			if (option.equals("credit")){
+
+				String query = "SELECT * FROM " + db_table + " WHERE username = ? AND PASSWORD = ?";
+				PreparedStatement pstmt = (PreparedStatement) conn.prepareStatement(query); 
+				pstmt.setString(1, username);
+				pstmt.setString(2, password);
+				ResultSet rs = pstmt.executeQuery();
+
+				if (rs.next()){
+					try {
+						double credit = Double.parseDouble(rs.getString("store_credit"));
+						if (credit > totalPrice2){
+							RequestDispatcher rd = request.getRequestDispatcher("congrats.jsp");
+							rd.forward(request, response);
+							/*
         				query = "UPDATE store_credit SET store_credit = " + (credit - getTotal(cart)) + " FROM " + db_table + " WHERE username = ?";
         				pstmt = (PreparedStatement) conn.prepareStatement(query); 
         				pstmt.setString(1, username);
         				pstmt.executeQuery();
-        				*/
-        			}
-        			else {
-            			//print error msg 
-            			out.print("<p style=\"color:red\">You didn't have enough store credit for the purchase. Please login again for security purposes.</p>");  
-            			//send user back to welcome page 
-        				RequestDispatcher rd=request.getRequestDispatcher("login.jsp");    
-        				rd.include(request,response);    
-        			}
-        		}
-        	}
-        	else{
-        		
-        		String query = "SELECT * FROM " + db_table + " WHERE username = ? AND PASSWORD = ?";
-        		PreparedStatement pstmt = (PreparedStatement) conn.prepareStatement(query); 
-        		pstmt.setString(1, username);
-        		pstmt.setString(2, password);
-        		ResultSet rs = pstmt.executeQuery();
-        		
-        		if (rs.next()){
-        			String credit = rs.getString("credit_card_info");
-        			if (cardNumber.equals(credit)){
-        				RequestDispatcher rd = request.getRequestDispatcher("congrats.jsp");
-        				rd.forward(request, response);
-        				/*
+							 */
+						}
+						else {
+							//print error msg 
+							out.print("<p style=\"color:red\">You didn't have enough store credit for the purchase. Please login again for security purposes.</p>");  
+							//send user back to welcome page 
+							RequestDispatcher rd=request.getRequestDispatcher("login.jsp");    
+							rd.include(request,response);    
+						}
+					}
+					catch (Exception e){
+						e.printStackTrace();
+					}
+				}
+			}
+			else{
+
+				String query = "SELECT * FROM " + db_table + " WHERE username = ? AND PASSWORD = ?";
+				PreparedStatement pstmt = (PreparedStatement) conn.prepareStatement(query); 
+				pstmt.setString(1, username);
+				pstmt.setString(2, password);
+				ResultSet rs = pstmt.executeQuery();
+
+				if (rs.next()){
+					String credit = rs.getString("credit_card_info");
+					if (cardNumber.equals(credit)){
+						RequestDispatcher rd = request.getRequestDispatcher("congrats.jsp");
+						rd.forward(request, response);
+						/*
         				query = "UPDATE store_credit SET store_credit = " + (credit - getTotal(cart)) + " FROM " + db_table + " WHERE username = ?";
         				pstmt = (PreparedStatement) conn.prepareStatement(query); 
         				pstmt.setString(1, username);
         				pstmt.executeQuery();
-        				*/
-        			}
-        			else {
-            			//print error msg 
-            			out.print("<p style=\"color:red\">There was a problem authenticating your credit card. Please login again for security purposes.</p>");  
-            			//send user back to welcome page 
-        				RequestDispatcher rd=request.getRequestDispatcher("login.jsp");    
-        				rd.include(request,response);   
-        			}
-        		}
-        	}
-        }
-        catch(SQLException se) {
-        	//handle jdbc errors
-        	se.printStackTrace(); 
-        }
-        catch(Exception e) {
-        	//handle Class.forName errors
-        	e.printStackTrace();
-        }
-        finally {
-        	//close resources
-        	out.close();
-        }
+						 */
+					}
+					else {
+						//print error msg 
+						out.print("<p style=\"color:red\">There was a problem authenticating your credit card. Please login again for security purposes.</p>");  
+						//send user back to welcome page 
+						RequestDispatcher rd=request.getRequestDispatcher("login.jsp");    
+						rd.include(request,response);   
+					}
+				}
+			}
+		}
+		catch(SQLException se) {
+			//handle jdbc errors
+			se.printStackTrace(); 
+		}
+		catch(Exception e) {
+			//handle Class.forName errors
+			e.printStackTrace();
+		}
+		finally {
+			//close resources
+			out.close();
+		}
 	}
-	
+
 	public static double getTotal(String book){
 		String[] x = book.split("/");
 		double total = 0;
@@ -146,7 +151,7 @@ public class CheckoutServlet extends HttpServlet {
 		}
 		System.out.println(total);
 		return total;
-		
-		
+
+
 	}
 }
